90beade0f52b49cc9505cdc7288310e3640d62c1
Manjesh Subramanyam <manjesh.subramanyam@gmail.com>
2018-03-16 01:52:38 +0530

    Implemented draft version of IPropertyMatcher Interface - IsMatch method

--- Domain_CodingExercise_Project/Classes/Helper.cs
+++ Domain_CodingExercise_Project/Classes/Helper.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Domain_CodingExercise_Project.Interfaces;
 
 namespace Domain_CodingExercise_Project.Classes
 {
-    class Helper
+    public class Helper:IPropertyMatcher
     {
+        public  bool IsMatch(Property agencyProperty, Property databaseProperty)
+        {
+            bool status = false;
+
+            return status;
+        }
     }
 }
