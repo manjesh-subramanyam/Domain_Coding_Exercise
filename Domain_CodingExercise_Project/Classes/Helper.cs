3184b0660b5848d0a5f74129e7b9cacb1954f750
Manjesh Subramanyam <manjesh.subramanyam@gmail.com>
2018-03-16 09:45:06 +0530

    Committing OTBRE and CRE Agency Code Logic

--- Domain_CodingExercise_Project/Classes/Helper.cs
+++ Domain_CodingExercise_Project/Classes/Helper.cs
@@ -1,17 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
 using Domain_CodingExercise_Project.Interfaces;
 
 namespace Domain_CodingExercise_Project.Classes
 {
-    public class Helper:IPropertyMatcher
+    public class Helper : IPropertyMatcher
     {
-        public  bool IsMatch(Property agencyProperty, Property databaseProperty)
+        public bool IsMatch(Property agencyProperty, Property databaseProperty)
         {
             bool status = false;
+            switch (agencyProperty.AgencyCode)
+            {
+                case "OTBRE":
+                    status = RemovePunctuation(agencyProperty.Name) == databaseProperty.Name
+                                          &&
+                             RemovePunctuation(agencyProperty.Address) == databaseProperty.Address;
+                    break;
+
+                case "LRE": break;
+
+                case "CRE":
+                    status = String.Join(" ", agencyProperty.Name.Split(' ').Reverse()) == databaseProperty.Name;
+                    break;
+            }
 
             return status;
+        }
+
+        private string RemovePunctuation(string source)
+        {
+            /*
+                \p{P}--> To replace punctuation with empty space
+                \s+ --> To remove extra spaces
+                Trim()--> To remove leading And trailing Spaces
+                */
+
+            return Regex.Replace(Regex.Replace(source, @"\p{P}", " "), @"\s+", " ").Trim();
         }
     }
 }
